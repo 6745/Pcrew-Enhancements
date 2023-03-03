8f5096f55c4bed583a877c105cead632862bc2e0
6745 <75388599+6745@users.noreply.github.com>
2023-03-03 10:56:29 +0000

    Add Points Calcuation

--- pcrew.user.js
+++ pcrew.user.js
@@ -23,66 +23,127 @@
 
 })();
 
+
 (function() {
     'use strict';
 
     const IMAGES = {
-        "/img/rank/nm_1.png": {value: 1, type: "Bronze"},
-        "/img/rank/nm_2.png": {value: 2, type: "Silver"},
-        "/img/rank/nm_3.png": {value: 3, type: "Gold"},
-        "/img/rank/nm_4.png": {value: 4, type: "Max"},
-        "/img/rank/hd_1.png": {value: 2, type: "Bronze"},
-        "/img/rank/hd_2.png": {value: 4, type: "Silver"},
-        "/img/rank/hd_3.png": {value: 6, type: "Gold"},
-        "/img/rank/hd_4.png": {value: 8, type: "Max"},
-        "/img/rank/mx_1.png": {value: 4, type: "Bronze"},
-        "/img/rank/mx_2.png": {value: 6, type: "Silver"},
-        "/img/rank/mx_3.png": {value: 12, type: "Gold"},
-        "/img/rank/mx_4.png": {value: 16, type: "Max"},
-        "/img/rank/ex_1.png": {value: 0, type: "Bronze"},
-        "/img/rank/ex_2.png": {value: 0, type: "Silver"},
-        "/img/rank/ex_3.png": {value: 0, type: "Gold"},
-        "/img/rank/ex_4.png": {value: 0, type: "Max"},
+        "/img/rank/nm_1.png": {points: 1, type: "Bronze",Difficulty:"NM"},
+        "/img/rank/nm_2.png": {points: 2, type: "Silver",Difficulty:"NM"},
+        "/img/rank/nm_3.png": {points: 3, type: "Gold",Difficulty:"NM"},
+        "/img/rank/nm_4.png": {points: 4, type: "Max",Difficulty:"NM"},
+        "/img/rank/hd_1.png": {points: 2, type: "Bronze",Difficulty:"HD"},
+        "/img/rank/hd_2.png": {points: 4, type: "Silver",Difficulty:"HD"},
+        "/img/rank/hd_3.png": {points: 6, type: "Gold",Difficulty:"HD"},
+        "/img/rank/hd_4.png": {points: 8, type: "Max",Difficulty:"HD"},
+        "/img/rank/mx_1.png": {points: 4, type: "Bronze",Difficulty:"MX"},
+        "/img/rank/mx_2.png": {points: 6, type: "Silver",Difficulty:"MX"},
+        "/img/rank/mx_3.png": {points: 12, type: "Gold",Difficulty:"MX"},
+        "/img/rank/mx_4.png": {points: 16, type: "Max",Difficulty:"MX"},
+        "/img/rank/ex_1.png": {points: 0, type: "Bronze",Difficulty:"EX"},
+        "/img/rank/ex_2.png": {points: 0, type: "Silver",Difficulty:"EX"},
+        "/img/rank/ex_3.png": {points: 0, type: "Gold",Difficulty:"EX"},
+        "/img/rank/ex_4.png": {points: 0, type: "Max",Difficulty:"EX"},
     };
 
+    let excludedSongs;
+if (window.location.href.includes("pop")) {
+    excludedSongs = [
+        {song: "Fermion", difficulties: ["MX"]},
+        {song: "Motion", difficulties: ["NM", "HD"]},
+        {song: "Hard To Start", difficulties: ["NM", "HD"]},
+        {song: "Mutu", difficulties: ["NM", "HD"]},
+        {song: "JBG", difficulties: ["NM", "HD"]},
+        {song: "CLOSER", difficulties: ["MX"]},
+        {song: "Coastal Tempo", difficulties: ["EX"]},
+        {song: "DIVINE SERVICE", difficulties: ["EX"]},
+        {song: "First Kiss", difficulties: ["EX"]},
+        {song: "Ladymade Star", difficulties: ["EX"]},
+        {song: "STOP", difficulties: ["EX"]},
+        {song: "Love is Beautiful", difficulties: ["EX"]},
+        {song: "Dream of Winds", difficulties: ["EX"]},
+        {song: "Thor", difficulties: ["MX"]},
+        {song: "The Night Stage", difficulties: ["MX"]},
+        {song: "DARK ENVY", difficulties: ["MX"]},
+        {song: "In my Dream", difficulties: ["EX"]},
+        {song: "Cypher Gate", difficulties: ["EX"]},
+        {song: "The Rain Maker", difficulties: ["EX"]},
+        {song: "Ghost", difficulties: ["EX"]},
+        {song: "SigNalize", difficulties: ["EX"]},
+        {song: "Dark Prism", difficulties: ["EX"]},
+        {song: "Pretty Girl", difficulties: ["EX"]},
+        {song: "SuperSonic 2011", difficulties: ["NM", "HD","MX"]},
+        {song: "In My Heart (Japanese Ver.)", difficulties: ["NM", "HD","MX"]},
+        {song: "The Clear Blue Sky (Japanese Ver.)", difficulties: ["NM", "HD","MX"]},
+        {song: "Step", difficulties: ["EX"]},
+        {song: "Memory of Wind", difficulties: ["NM", "HD","MX"]},
+    ];
+} else if (window.location.href.includes("star")) {
+    excludedSongs = [
+        {song: "Motion", difficulties: ["NM"]},
+        {song: "PARA-Q", difficulties: ["NM"]},
+        {song: "JBG", difficulties: ["NM"]},
+        {song: "Hard To Start", difficulties: ["NM"]},
+        {song: "Mutu", difficulties: ["MM"]},
+        {song: "SuperSonic 2011", difficulties: ["MN"]},
+        {song: "Memory of Wind", difficulties: ["NM"]},
+        {song: "The Clear Blue Sky (Japanese Ver.)", difficulties: ["NM"]},
+        {song: "In My Heart (Japanese Ver.)", difficulties: ["NM"]},
+
+    ];
+} else {
+    excludedSongs = [];
+}
+
+    let totalPoints = 0;
     let bronzeCount = 0;
     let silverCount = 0;
     let goldCount = 0;
     let maxCount = 0;
-
 
     // Find all images on the page and count the number of occurrences of each one
     const imageElements = document.querySelectorAll("img");
     const imageCounts = {};
     imageElements.forEach((img) => {
         const src = img.getAttribute("src");
-        if (src in IMAGES) {
-            const value = IMAGES[src].value;
-            const type = IMAGES[src].type;
-            imageCounts[src] = (imageCounts[src] || 0) + 1;
-            if (type === "Bronze") {
-                bronzeCount += 1;
-            } else if (type === "Silver") {
-                silverCount += 1;
-            } else if (type === "Gold") {
-                goldCount += 1;
-            } else if (type === "Max") {
-                maxCount += 1;
-            }
+        const tableRow = img.closest("tr");
+        if (tableRow) {
+            const songName = tableRow.querySelector("td:first-child").textContent.trim(); // get song name from table row
+            if (src in IMAGES) {
+    const songName = tableRow.querySelector("td:first-child").textContent.trim(); // get song name from table row
+    const difficulty = IMAGES[src].Difficulty; // get difficulty from image object
+    const excludedDifficulties = excludedSongs.find((excludedSong) => excludedSong.song === songName)?.difficulties || []; // find excluded difficulties for current song
+    if (!excludedDifficulties.includes(difficulty)) { // check if image is valid and difficulty is not excluded
+        const points = IMAGES[src].points;
+        const type = IMAGES[src].type;
+        imageCounts[src] = (imageCounts[src] || 0) + 1;
+        if (type === "Bronze") {
+            bronzeCount += 1;
+        } else if (type === "Silver") {
+            silverCount += 1;
+        } else if (type === "Gold") {
+            goldCount += 1;
+        } else if (type === "Max") {
+            maxCount += 1;
+        }
+        totalPoints += points;
+    }
+}
+
         }
     });
 
- // Append the total score and count of each type of rank to the specified div element
-const progressWrapper = document.querySelector("div.progress-wrapper.section");
-const totalScoreElement = document.createElement("span");
-totalScoreElement.style.display = "block"; // Add this line to make it a block-level element
-progressWrapper.appendChild(totalScoreElement);
-const rankCountsElement = document.createElement("span");
+    // Append the total score and count of each type of rank to the specified div element
+    const progressWrapper = document.querySelector("div.progress-wrapper.section");
+    const totalScoreElement = document.createElement("span");
     totalScoreElement.style.display = "block";
-rankCountsElement.textContent = `Bronze: ${bronzeCount}, Silver: ${silverCount}, Gold: ${goldCount}, Max Combos: ${maxCount}`;
-//rankCountsElement.style.display = "block"; // Add this line to make it a block-level element
-rankCountsElement.style.marginTop = "10px"; // Add this line to add a margin between the two elements
-progressWrapper.appendChild(rankCountsElement);
+    totalScoreElement.textContent = `Ranking Points: ${totalPoints}`;
+    progressWrapper.appendChild(totalScoreElement);
+
+    const rankCountsElement = document.createElement("span");
+    rankCountsElement.style.display = "block";
+    rankCountsElement.textContent = `Bronze: ${bronzeCount}, Silver: ${silverCount}, Gold: ${goldCount}, Max Combos: ${maxCount}`;
+    progressWrapper.appendChild(rankCountsElement);
 })();
 
 (function() {
